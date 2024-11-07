570e7a92b918d9d53abe39e7f077fa08dff9455d
Daniel-WORK-GH <120199463+Daniel-WORK-GH@users.noreply.github.com>
2024-11-07 09:09:44 +0200

    Added functions

--- teaching_data_structures/Stack/StackList.cs
+++ teaching_data_structures/Stack/StackList.cs
@@ -1,13 +1,14 @@
-using System.ComponentModel.DataAnnotations;
 using DataStructure.LinkedList;
 
 class StackList<T>
 {
     Node<T>? lastnode;
 
+    int size;
+
     public StackList()
     {
-
+        size = 0;
     }
 
     public void Push(T value)
@@ -22,18 +23,74 @@ class StackList<T>
             node.SetNext(lastnode);
             lastnode = node;
         }
+        size++;
     }
 
     public T Pop()
     {
+        if(lastnode == null) 
+        {
+            throw new Exception("Can't pop - the stack is empty.");
+        }
+
         T result = lastnode.GetValue();
         lastnode = lastnode.GetNext();
+        size--;
         return result;
     }
 
     public T Peek()
     {
+        if(lastnode == null) 
+        {
+            throw new Exception("Can't peek - the stack is empty.");
+        }
+
         T result = lastnode.GetValue();
+        return result;
+    }
+
+    public int GetSize() 
+    {
+        return size;
+    }
+
+    public bool IsEmpty() 
+    {
+        return size == 0;
+    }
+
+    public bool Contains(T value) 
+    {
+        Node<T>? temp = lastnode;
+
+        while(temp != null)
+        {
+            T? tempvalue = temp.GetValue(); 
+            
+            if((tempvalue != null && tempvalue.Equals(value)) || (tempvalue == null && value == null))
+            {
+                return true;
+            }
+
+            temp = temp.GetNext();
+        }
+
+        return false;
+    }
+
+    public StackList<T> Reverse() 
+    {
+        StackList<T> result = new StackList<T>();
+        Node<T>? temp = lastnode;
+
+        while(temp != null)
+        {
+            T? tempvalue = temp.GetValue(); 
+            result.Push(tempvalue);
+            temp = temp.GetNext();
+        }
+
         return result;
     }
 }
