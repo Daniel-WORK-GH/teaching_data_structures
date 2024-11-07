6df29169deb67363f53f8b1c8c4522e8f76afb29
Daniel-WORK-GH <120199463+Daniel-WORK-GH@users.noreply.github.com>
2024-11-07 09:09:25 +0200

    Added functions

--- teaching_data_structures/Queue/QueueList.cs
+++ teaching_data_structures/Queue/QueueList.cs
@@ -2,12 +2,14 @@ using DataStructure.LinkedList;
 
 class QueueList<T>
 {
-    Node<T> first;
-    Node<T> last;
+    Node<T>? first;
+    Node<T>? last;
+
+    int size;
 
     public QueueList()
     {
-
+        size = 0;
     }
 
     public void Enqueue(T value)
@@ -20,21 +22,67 @@ class QueueList<T>
         else
         {
             Node<T> node = new Node<T>(value);
-            last.SetNext(node);
+            last!.SetNext(node);
             last = node;
         }
+        size++;
     }
 
     public T Dequeue()
     {
+        if(first == null) 
+        {
+            throw new Exception("Can't dequeue - the queue is empty.");
+        }
+
         T result = first.GetValue();
         first = first.GetNext();
+        size--;
         return result;
     }
 
     public T Peek()
     {
+        if(first == null) 
+        {
+            throw new Exception("Can't peek - the queue is empty.");
+        }
+
         T result = first.GetValue();
         return result;
+    }
+
+    public int GetSize()
+    {
+        return size;
+    }
+
+    public bool IsEmpty() 
+    {
+        return size == 0;
+    }
+
+    public bool Contains(T value) 
+    {
+        Node<T>? temp = first;
+
+        while(temp != null)
+        {
+            T? tempvalue = temp.GetValue(); 
+            
+            if((tempvalue != null && tempvalue.Equals(value)) || (tempvalue == null && value == null))
+            {
+                return true;
+            }
+
+            temp = temp.GetNext();
+        }
+
+        return false;
+    }
+
+    public QueueList<T> Reverse() 
+    {
+        throw new NotImplementedException("Not implemented yet.");
     }
 }
